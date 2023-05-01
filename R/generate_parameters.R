1831a36b1d5dc39635ed5a50e08c195325f5e98c
Hisham Eldardiry <35979167+HishamEldardiry@users.noreply.github.com>
2023-04-30 18:52:35 -0700

    Add files via upload

--- R/generate_parameters.R
+++ R/generate_parameters.R
@@ -31,7 +31,7 @@ generate_parameters <- function(year = 2009,
           left_join(read_HydroSource(data_dir=data_dir) %>%
                       select(EIA_ID, nameplate_HS = CH_MW, plant, state, bal_auth, mode),
                     by = "EIA_ID") %>% mutate(month = match(month, month.abb)) %>%
-          append_capabilities("monthly") %>%
+          append_capabilities(data_dir=data_dir,"monthly") %>%
           mutate(capability = if_else(is.na(capability),nameplate_EIA, capability)) %>%
           mutate(pmean_MW = if_else(pmean_MW > capability, capability, pmean_MW)) %>%
           mutate(pmax = pmean_MW + (max_param * (capability - pmean_MW)),
@@ -46,7 +46,7 @@ generate_parameters <- function(year = 2009,
           left_join(pmean_monthly_x, by = c("EIA_ID", "month")) %>%
           left_join(read_HydroSource(data_dir=data_dir) %>% select(EIA_ID, nameplate_HS = CH_MW, plant, state, bal_auth, mode),
                     by = "EIA_ID") %>% mutate(month = match(month, month.abb)) %>%
-          append_capabilities("monthly") %>%
+          append_capabilities(data_dir=data_dir,"monthly") %>%
           mutate(pmax = pmean_MW + (max_param * (capability - pmean_MW)),
                  pmin = pmean_MW * min_param) %>%
           select(EIA_ID, plant, state, bal_auth, mode, year, month,
@@ -78,7 +78,7 @@ generate_parameters <- function(year = 2009,
       left_join(get_pmax_pmin_predictions(), by = "EIA_ID") %>%
       left_join(read_HydroSource(data_dir=data_dir) %>% select(EIA_ID, nameplate_HS = CH_MW, plant, state, bal_auth, mode),
                 by = "EIA_ID") %>%
-      append_capabilities("weekly") %>%
+      append_capabilities(data_dir=data_dir,"weekly") %>%
       mutate(capability = if_else(is.na(capability), nameplate_EIA, capability)) %>%
       mutate(pmean_MW = if_else(pmean_MW > capability, capability, pmean_MW)) %>%
       mutate(pmax = pmean_MW + (max_param * (capability - pmean_MW)),
@@ -91,7 +91,7 @@ generate_parameters <- function(year = 2009,
       left_join(read_HydroSource(data_dir=data_dir) %>%
                   select(EIA_ID, nameplate_HS = CH_MW, plant, state, bal_auth, mode),
                 by = "EIA_ID") %>%
-      append_capabilities("weekly") %>%
+      append_capabilities(data_dir=data_dir,"weekly") %>%
       left_join(pmean_weekly_x, by = c("EIA_ID", "epiweek")) %>%
       rowwise() %>%
       mutate(
